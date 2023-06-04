f0c7ace5893de1e1f086f14c10448a5bebebcb55
Arman <37100541+CarboxyDev@users.noreply.github.com>
2023-06-04 08:19:20 +0530

    Update gitignore + Remove node_modules

--- src/main/java/me/carboxydev/main/enchants/ModEnchants.java
+++ src/main/java/me/carboxydev/main/enchants/ModEnchants.java
@@ -9,25 +9,26 @@ import net.minecraft.util.registry.Registry;
 
 public class ModEnchants {
 
-  public static Enchantment HOLLOWBITE_ENCHANTMENT = new HollowbiteEnchantment(
-    Enchantment.Rarity.RARE,
-    EnchantmentTarget.WEAPON,
-    new EquipmentSlot[] { EquipmentSlot.MAINHAND }
-  );
+    public static Enchantment HOLLOWBITE_ENCHANTMENT =
+        new HollowbiteEnchantment(
+            Enchantment.Rarity.RARE,
+            EnchantmentTarget.WEAPON,
+            new EquipmentSlot[] { EquipmentSlot.MAINHAND }
+        );
 
-  public static void registerEnchantments() {
-    registerEnchantment("hollowbite_enchantment", HOLLOWBITE_ENCHANTMENT);
-  }
+    public static void registerEnchantments() {
+        registerEnchantment("hollowbite_enchantment", HOLLOWBITE_ENCHANTMENT);
+    }
 
-  private static Enchantment registerEnchantment(
-    String name,
-    Enchantment enchant
-  ) {
-    DemoMod.LOGGER.debug("Registering Mod Enchants for" + DemoMod.MOD_ID);
-    return Registry.register(
-      Registry.ENCHANTMENT,
-      new Identifier(DemoMod.MOD_ID, name),
-      enchant
-    );
-  }
+    private static Enchantment registerEnchantment(
+        String name,
+        Enchantment enchant
+    ) {
+        DemoMod.LOGGER.debug("Registering Mod Enchants for" + DemoMod.MOD_ID);
+        return Registry.register(
+            Registry.ENCHANTMENT,
+            new Identifier(DemoMod.MOD_ID, name),
+            enchant
+        );
+    }
 }
